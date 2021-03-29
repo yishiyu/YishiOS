9fe7cf67c0ff5dd8bd1deddeb84d5f2c85f4714e
yishiyu <814384981@qq.com>
2021-03-29 14:13:14 +0800

    0.2.1 使用引导区运行,准备构建启动扇区

--- code/boot.asm
+++ code/boot.asm
@@ -1,6 +1,6 @@
 
-	org	0100h			; 告诉编译器程序加载到0100h处
-	mov	ax, cs			; 因为低地址会用来存放返回要用的地址参数等
+	org	07c00h			; 告诉编译器程序加载到7c00处
+	mov	ax, cs			; 之所以是7c00和bios有关
 	mov	ds, ax
 	mov	es, ax
 	
@@ -9,11 +9,10 @@
 	mov	bp, ax			; ES:BP = 串地址
 	mov	cx, 16			; CX = 串长度
 	mov	ax, 01301h		; AH = 13,  AL = 01h
-	mov	bx, 000ch		  ; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
+	mov	bx, 000ch		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
 	mov	dl, 0
-	int	10h					; 10h 号中断
-	xor ax,ax			  ; 返回DOS
-	int 21h
+	int	10h			; 10h 号中断
+	jmp $			; 停机
 
 
 BootMessage:		db	"Hello, OS world!"	;输出信息
